fc5b5329a3530b29559651baa633b892c58584a8
yuselenin <you@example.com>
2017-11-15 10:36:09 -0500

    s

---
+++ animal/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Animal(models.Model):
+    """docstring for Animal"""
+    name = models.CharField(max_length=30)
+    sound = models.CharField(max_length=30)
+
+    class meta:
+        verbose_name = "Animal"
+        verbose_name_plural = "Animals"
+
+    def __strs_(self):
+        return self.name
+
+    def speak(self):
+        return 'The %s says "%s"' % (self.name, self.sound)
