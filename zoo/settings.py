38f9e9859c91e1902a103e367edebaaa7e54eef5
yuselenin <you@example.com>
2017-11-15 12:15:55 -0500

    s

--- zoo/settings.py
+++ zoo/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = INSTALLED_APPS + PROJECTS_APPS
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_pep8',
     'django_jenkins.tasks.run_pyflakes',
+    'django_jenkins.tasks.run_pylint',
     # 'django_jenkins.tasks.run_jslint',
     # 'django_jenkins.tasks.run_csslint',
     # 'django_jenkins.tasks.run_sloccount'
